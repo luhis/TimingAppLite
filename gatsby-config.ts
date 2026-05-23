80ac9a59b27df7dba1f79043e0a5aa956672686d
Matt McCorry <mccorry@gmail.com>
2026-05-23 22:25:21 +0100

    fix: update siteUrl in Gatsby configuration to correct domain

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,10 +4,11 @@ const config: GatsbyConfig = {
   siteMetadata: {
     title: "Timing App Lite",
     description: "Gatsby TypeScript leaderboard app backed by the Sapphire Solutions autotest API.",
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://timingapplite.mccorry.dev",
   },
   graphqlTypegen: true,
   plugins: [
+    `gatsby-plugin-preact`,
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
