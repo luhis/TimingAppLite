c660cbfcd0eafdbcc01c6a4d2412a852c506abcf
Matt McCorry <mccorry@gmail.com>
2026-06-01 22:13:14 +0100

    Add health checks and /healthz endpoint
    Added health check services to the DI container and mapped a /healthz endpoint for external health monitoring. This enables systems to verify application health status.

--- DotNetBackend/DotNetBackend/Program.cs
+++ DotNetBackend/DotNetBackend/Program.cs
@@ -33,6 +33,7 @@ builder.Services.AddCors(options =>
             .AllowCredentials()));
 
 builder.Services.AddOpenApi();
+builder.Services.AddHealthChecks();
 
 builder.Services.AddSignalR()
     .AddJsonProtocol(options =>
@@ -45,6 +46,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseResponseCompression();
 app.UseCors("AllowedOrigins");
+app.MapHealthChecks("/healthz");
 app.MapHub<LeaderboardHub>("/hubs/LeaderBoard").RequireCors("AllowedOrigins");
 app.MapGet("/API/1/LiveAllCompetitions", (IApiClient api) => api.GetLiveAllCompetitions());
 app.MapGet("/API/1/Competitions/{competionId:int}/LeaderBoards/{leaderboardId:int?}", (IApiClient api, int competionId, int? leaderboardId) => api.GetLeaderboards(competionId, leaderboardId));
